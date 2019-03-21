40daf3f9126620a8e917ee81f5296b99f4644934
Mark Rafter <S00181793@mail.itsligo.ie>
2019-03-21 09:22:17 +0000

    Add componts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SuiModule} from 'ng2-semantic-ui';
 
 import { AppComponent } from './app.component';
+import { CarlistComponent } from './carlist/carlist.component';
+import { CarComponent } from './car/car.component';
+import { AddCarComponent } from './add-car/add-car.component';
+import { AngularFirestoreModule} from '@angular/fire/firestore';
+import { AngularFireModule} from '@angular/fire';
+
+import { HttpClientModule } from '@angular/common/http';
+import { environment} from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarlistComponent,
+    CarComponent,
+    AddCarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    SuiModule,
+    AngularFireModule.initializeApp(environment.firebase,),
+    AngularFirestoreModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/car/car.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+  @Input() carData:ICar;
+  carImageWidth:number = 300;
+  
+  constructor() { }
+
+  
+  addReview(review:string):boolean{
+    console.log("CarComponent: addReview: review =" +review)
+    this.carData.myReview = review;
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
